2c6f2b3f440ec57a1411bd50d3e4b0702c90203c
Manjeetyadav29 <yadavmanjeet2001@gmail.com>
2026-02-24 00:21:28 +0530

    remove angular cache and dist from tracking

--- dist/manjeet-yadav-portfolio/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-3M6QBTXN.mjs";import"./chunk-NUJ4IAWE.mjs";import"./chunk-UNJ4R7WL.mjs";import"./chunk-DKCWOBCA.mjs";import"./chunk-FLOQCTOY.mjs";import"./chunk-KGCZWDNS.mjs";import"./chunk-6E4HMZ5K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
